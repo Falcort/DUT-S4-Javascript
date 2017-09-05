e68ebc986c2bb2144d5b9cd0eb70223244be1d04
Falcort <falcort.jeux@gmail.com>
2017-09-05 22:05:00 +0200

    Intro : Formulaire = validation de la form

--- Extercice formulaire/script.js
+++ Extercice formulaire/script.js
@@ -1,79 +1,109 @@
-function creat_form()
+function create_form()
 {
-    document.getElementById('age').addEventListener('input', fun_age);
-    document.getElementById('id').addEventListener('input', fun_id);
-    document.getElementById('password').addEventListener('input', fun_pwd);
-    document.getElementById('passwordRe').addEventListener('input', fun_pwd_re);
-    document.getElementById('cgu').addEventListener('change', fun_cgu);
+    document.getElementById('age').addEventListener('input', validate_age);
+    document.getElementById('id').addEventListener('input', validate_id);
+    document.getElementById('password').addEventListener('input', validate_pwd);
+    document.getElementById('passwordRe').addEventListener('input', validate_pwdRe);
+    document.getElementById('cgu').addEventListener('change', validate_cgu);
 }
 
-window.addEventListener("load", creat_form);
+window.addEventListener("load", create_form);
 
-var errors = 0;
 
-function fun_age()
+var g_age, g_id, g_pwd, g_pwdRe, g_cgu;
+
+function validate_age()
 {
     var age = document.getElementById('age').value;
     if(age < 18)
     {
-        console.log("Trop jeune");
-        errors++;
+        g_age = false;
     }
     else
     {
-        console.log("Age ok");
-        errors--;
+        g_age = true;
     }
+    validate_form()
 }
 
-function fun_id()
+function validate_id()
 {
     var id = document.getElementById('id').value;
     if(id.length > 12 || id.length <2 || !/^[a-zA-Z]+$/.test(id))
     {
-        console.log("Id non valide");
-        errors++;
+        g_id = false;
     }
     else
     {
-        console.log("Id ok");
-        errors--;
+        g_id = true;
     }
+    validate_form()
 }
 
-function fun_pwd()
+function validate_pwd()
 {
-    var pwd = document.getElementById('id').value;
+    var pwd = document.getElementById('password').value;
+
+    if(pwd.length > 30 || pwd.length <8 || !/^[a-zA-Z]+$/.test(pwd))
+    {
+        g_pwd = false;
+        document.getElementById('passwordRe').disabled = true;
+    }
+    else
+    {
+        document.getElementById('passwordRe').disabled = false;
+        g_pwd = true;
+    }
+    validate_form()
 }
 
-function fun_pwd_re()
+function validate_pwdRe()
 {
     var pwd = document.getElementById('password').value;
     var pwd_re = document.getElementById('passwordRe').value;
 
     if(pwd === pwd_re)
     {
-        console.log('Confirmation ok');
-        errors--;
+        g_pwdRe = true;
     }
     else
     {
-        console.log('Confimation pas ok');
-        errors++;
+        g_pwdRe = false;
     }
+    validate_form()
 }
 
-function fun_cgu()
+function validate_cgu()
 {
     var cgu = document.getElementById('cgu').checked;
-    if(cgu === true)
+
+    if(cgu)
     {
-        console.log("cgu ok");
-        errors--;
+        g_cgu = true
     }
     else
     {
-        console.log("cgu pas ok");
-        errors++;
+        g_cgu = false;
     }
+    validate_form()
+}
+
+function validate_form()
+{
+    if(g_age && g_id && g_pwd && g_pwdRe && g_cgu)
+    {
+        document.getElementById('submit').disabled = false;
+    }
+    else
+    {
+        document.getElementById('submit').disabled = true;
+    }
+    console.log(("-----------------------"));
+    console.log("age = " + g_age);
+    console.log("id = " + g_id);
+    console.log("pwd = " + g_pwd);
+    console.log("pwdRe = " + g_pwdRe);
+    console.log("cgu " + g_cgu);
+    console.log(("-----------------------"));
+
 }
