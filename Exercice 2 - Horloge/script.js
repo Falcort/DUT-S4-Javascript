1552897f9ec8bed0adf21ca0d558eef9f809865d
Falcort <falcort.jeux@gmail.com>
2017-09-18 09:28:41 +0200

    Horloge fonctionnelle

--- Exercice 2 - Horloge/script.js
+++ Exercice 2 - Horloge/script.js
@@ -5,3 +5,47 @@ var secondes = time.getSeconds();
 //document.getElementById("time").textContent = time;
 document.getElementById("time").textContent = heurs + ":" + minutes + ":" + secondes;
 
+window.addEventListener("load", uneSeconde);
+function uneSeconde()
+{
+    setInterval
+    (
+        function()
+        {
+            secondes++;
+            if(secondes === 60)
+            {
+                minutes++;
+                secondes = 0;
+            }
+            if(minutes === 60)
+            {
+                heurs++;
+                minutes = 0;
+            }
+            if(heurs === 24 && minutes === 60 && secondes ===60)
+            {
+                heurs = 00;
+            }
+
+            if(minutes < 10)
+            {
+                minutes = "0" + minutes;
+            }
+
+            if(secondes < 10)
+            {
+                secondes = "0" + secondes;
+            }
+
+            if(heurs < 10)
+            {
+                heurs = "0" + heurs;
+            }
+
+            document.getElementById("time").textContent = heurs + ":" + minutes + ":" + secondes;
+        },
+        1000
+    );
+
+}
